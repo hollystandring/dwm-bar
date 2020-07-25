70b8df907bdbe3a153fcd943a26a84ee1ec1ee62
Joe Standring <git@joestandring.com>
2020-07-25 17:12:58 +0100

    Add wireguard support for dwm_vpn

--- bar-functions/dwm_vpn.sh
+++ bar-functions/dwm_vpn.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
 
-# A dwm_bar function to show VPN connections (if any are active)
+# A dwm_bar function to show VPN connections with OpenVPN or WireGuard (if any are active)
 # Joe Standring <git@joestandring.com>
 # GNU GPLv3
 
-# Dependencies: NetworkManager-openvpn
+# Dependencies: NetworkManager, NetworkManager-openvpn (for OpenVPN connections)
 
 dwm_vpn () {
     VPN=$(nmcli -a | grep 'VPN connection' | sed -e 's/\( VPN connection\)*$//g')
+    
+    if [ "$VPN" = "" ]; then
+        VPN=$(nmcli connection | grep 'wireguard' | sed 's/\s.*$//')
+    fi
 
     if [ "$VPN" != "" ]; then
         printf "%s" "$SEP1"
